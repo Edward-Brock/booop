5c32d70a2a774f3b15dee3ac9f6b6ff117dedf16
Edward Brock <1416477069@qq.com>
2024-12-22 21:24:46 +0800

    chore: 安装 @nuxt/ui 依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
   eslint: {
